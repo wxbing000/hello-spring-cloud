73107af55063ee5f1817d9ff73764ed2b533eb50
wxb <wxbing000@163.com>
2017-06-08 15:54:22 +0800

    add config client to test read config

---
+++ hello-config-client/src/main/java/com/wxb/config/client/ConfigClientApplication.java
@@ -0,0 +1,31 @@
+package com.wxb.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * describe:
+ *
+ * @author : 王校兵
+ * @version : v1.0
+ * @time : 2017/6/8  15:50
+ */
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
